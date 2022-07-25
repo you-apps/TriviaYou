c9ce70d8d9ec41945a3953d2727c4fdae1277fe3
Bnyro <yannick.fehler@web.de>
2022-07-25 11:31:52 +0200

    style about fragment

---
+++ app/src/main/java/com/bnyro/trivia/Constants.kt
@@ -0,0 +1,5 @@
+package com.bnyro.trivia
+
+const val GITHUB_URL = "https://github.com/Bnyro/TriviaYou"
+const val LICENSE_URL = "https://gnu.org"
+const val API_URL = "https://the-trivia-api.com"
--- app/src/main/java/com/bnyro/trivia/activities/AboutActivity.kt
+++ app/src/main/java/com/bnyro/trivia/activities/AboutActivity.kt
@@ -1,17 +1,46 @@
 package com.bnyro.trivia.activities
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import com.bnyro.trivia.API_URL
+import com.bnyro.trivia.BuildConfig
+import com.bnyro.trivia.GITHUB_URL
+import com.bnyro.trivia.LICENSE_URL
 import com.bnyro.trivia.databinding.ActivityAboutBinding
 
 class AboutActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAboutBinding
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
         binding = ActivityAboutBinding.inflate(layoutInflater)
+
+        binding.back.setOnClickListener {
+            onBackPressed()
+        }
+        binding.version.text = BuildConfig.VERSION_NAME
+
+        binding.github.setOnClickListener {
+            openBrowserIntent(GITHUB_URL)
+        }
+
+        binding.api.setOnClickListener {
+            openBrowserIntent(API_URL)
+        }
+
+        binding.license.setOnClickListener {
+            openBrowserIntent(LICENSE_URL)
+        }
+
         setContentView(binding.root)
+    }
+
+    private fun openBrowserIntent(url: String) {
+        val browserIntent = Intent(Intent.ACTION_VIEW)
+        browserIntent.data = Uri.parse(url)
+        startActivity(browserIntent)
     }
 }
