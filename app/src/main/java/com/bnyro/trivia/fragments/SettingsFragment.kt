bc6745141a567974e8291d713e579b1d79699d69
Bnyro <yannick.fehler@web.de>
2022-09-09 10:01:50 +0200

    fixes

--- app/src/main/java/com/bnyro/trivia/fragments/SettingsFragment.kt
+++ app/src/main/java/com/bnyro/trivia/fragments/SettingsFragment.kt
@@ -2,9 +2,11 @@ package com.bnyro.trivia.fragments
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.ListPreference
 import androidx.preference.Preference
 import com.bnyro.trivia.R
 import com.bnyro.trivia.extensions.PrefFragment
+import com.bnyro.trivia.util.ApiInstance
 import com.bnyro.trivia.util.BackupHelper
 import com.bnyro.trivia.util.PreferenceHelper
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
@@ -12,6 +14,14 @@ import com.google.android.material.dialog.MaterialAlertDialogBuilder
 class SettingsFragment : PrefFragment() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
+
+        val api = findPreference<ListPreference>(context?.getString(R.string.api_key)!!)
+        api?.setOnPreferenceChangeListener { _, newValue ->
+            ApiInstance.updateApiHelper(
+                PreferenceHelper.getApi()
+            )
+            true
+        }
 
         val resetStats = findPreference<Preference>(context?.getString(R.string.reset_stats_key)!!)
         resetStats?.setOnPreferenceClickListener {
--- app/src/main/java/com/bnyro/trivia/util/ApiInstance.kt
+++ app/src/main/java/com/bnyro/trivia/util/ApiInstance.kt
@@ -5,10 +5,19 @@ import com.bnyro.trivia.api.thetriviaapi.TheTriviaApiHelper
 import com.bnyro.trivia.obj.ApiType
 
 object ApiInstance {
-    private val apiPref = PreferenceHelper.getApi()
-    var apiHelper = when (apiPref) {
+    var apiHelper = when (
+        PreferenceHelper.getApi()
+    ) {
         ApiType.theTriviaApi -> TheTriviaApiHelper()
         ApiType.openTriviaApi -> OpenTriviaDBHelper()
         else -> TheTriviaApiHelper()
+    }
+
+    fun updateApiHelper(apiPref: Int) {
+        apiHelper = when (apiPref) {
+            ApiType.theTriviaApi -> TheTriviaApiHelper()
+            ApiType.openTriviaApi -> OpenTriviaDBHelper()
+            else -> TheTriviaApiHelper()
+        }
     }
 }
